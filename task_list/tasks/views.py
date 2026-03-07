2e40e28c542bcbf29a4b3c738f3b122ee178f4ad
CatherineP37 <137174676+CatherineP37@users.noreply.github.com>
2026-03-07 08:23:40 +0000

    Add home page, view and url

--- task_list/tasks/views.py
+++ task_list/tasks/views.py
@@ -1,2 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import TaskList
+from .forms import AddTask
+
+def home(request):
+    tasks = TaskList.objects.all()
+    form = AddTask()
+    if request.method == 'POST':
+        form = AddTask(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')        
+    context = {'form':form, 'tasks':tasks}
+    return render(request, 'home.html', context)
